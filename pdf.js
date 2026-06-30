3cbe5210c52c4a941c20fbd0a5b545844d0b9952
zarubinscky <zarubinscky@gmail.com>
2026-06-30 18:32:24 +0300

    Create pdf.js

---
+++ pdf.js
@@ -0,0 +1,27 @@
+/*
+
+ * MeetMind AI
+
+ * Executive PDF Generator
+
+ *
+
+ * В этом файле будет вся логика генерации Executive PDF.
+
+ * Пока файл является заготовкой.
+
+ */
+
+'use strict';
+
+const PDF_CONFIG = {
+
+    version: '1.0',
+
+    orientation: 'landscape',
+
+    ratio: '16:9'
+
+};
+
+// Здесь позже появится вся логика PDF
