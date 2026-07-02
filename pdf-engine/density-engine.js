66086835cca78362a3bf6979d3e227169fdf37b0
zarubinscky <zarubinscky@gmail.com>
2026-07-02 13:01:15 +0300

    Update density-engine.js

--- pdf-engine/density-engine.js
+++ pdf-engine/density-engine.js
@@ -1,1 +1,14 @@
-// MeetMind PDF Engine
+(function () {
+    "use strict";
+
+    window.DensityEngine = {
+
+        optimize(layout) {
+
+            return layout;
+
+        }
+
+    };
+
+})();
