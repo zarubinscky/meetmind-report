854ef8a86a79ee88e7912f9cc2b169d25d347024
zarubinscky <zarubinscky@gmail.com>
2026-07-02 14:29:35 +0300

    Update layout-search-engine.js

--- pdf-engine/layout-search-engine.js
+++ pdf-engine/layout-search-engine.js
@@ -1,1 +1,17 @@
+(function () {
 
+    "use strict";
+
+    window.LayoutSearchEngine = {
+
+        search(layout) {
+
+            return layout;
+
+        }
+
+    };
+
+    console.log("✅ Layout Search Engine loaded.");
+
+})();
