2181fc3caae3b267179242c69650570e71deb1e7
zarubinscky <zarubinscky@gmail.com>
2026-07-02 12:52:11 +0300

    Update index.js

--- pdf-engine/index.js
+++ pdf-engine/index.js
@@ -1,23 +1,16 @@
 (function () {
-
     "use strict";
 
     window.MeetMindPDF = {
-
         version: "1.0.0",
 
         generate(reportData) {
-
             console.log("🚀 MeetMind PDF Engine v1.0");
-
             console.log(reportData);
 
             alert("MeetMind PDF Engine connected!");
-
         }
-
     };
 
     console.log("✅ MeetMind PDF Engine loaded.");
-
 })();
