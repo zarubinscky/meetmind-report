bc920ee07b05c1e6b44baaa38b6da07873ba3bcf
zarubinscky <zarubinscky@gmail.com>
2026-07-03 11:33:19 +0300

    Update helpers.js

--- pdf-engine/renderers/helpers.js
+++ pdf-engine/renderers/helpers.js
@@ -13,8 +13,18 @@
                 .replace(/"/g, "&quot;")
                 .replace(/'/g, "&#039;");
 
-        }
+        },
 
-    };
+         
+        section(title, content, className = ""){
+    return `
+        <section class="mm-pdf-section ${className}">
+            ${title ? `<h2 class="mm-pdf-section-title">${this.escape(title)}</h2>` : ""}
+            ${content || ""}
+        </section>
+    `;
+}
+
+};
 
 })();
