ac13d29cefa42fcec5d393f734102da3c3a5853f
zarubinscky <zarubinscky@gmail.com>
2026-07-03 11:25:43 +0300

    Update helpers.js

--- pdf-engine/renderers/helpers.js
+++ pdf-engine/renderers/helpers.js
@@ -4,6 +4,17 @@
 
     window.RenderHelpers = {
 
+        escape(value){
+
+            return String(value ?? "")
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#039;");
+
+        }
+
     };
 
 })();
