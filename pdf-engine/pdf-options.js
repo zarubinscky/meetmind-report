a649993cbc52be5e212f0ee7a51dea00c7f0b321
zarubinscky <zarubinscky@gmail.com>
2026-07-04 20:48:53 +0300

    Create pdf-options.js

---
+++ pdf-engine/pdf-options.js
@@ -0,0 +1,13 @@
+const DEFAULT_PDF_OPTIONS = {
+
+    header: true,
+    statistics: true,
+    summary: true,
+    findings: true,
+    tasks: true,
+    owners: true,
+    architecture: true,
+    footer: true
+};
+
+window.DEFAULT_PDF_OPTIONS = DEFAULT_PDF_OPTIONS;
