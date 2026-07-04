f42b83a99658aea1ce07cdbb60a4e3d92ed5e9b7
zarubinscky <zarubinscky@gmail.com>
2026-07-04 22:00:36 +0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -5,9 +5,12 @@
 let currentModal = null;
 
 function show(config) {
-
+    close();
+    const overlay = document.createElement("div");
+    overlay.className = "mm-modal-overlay";
+    currentModal = overlay;
+    document.body.appendChild(overlay);
     console.log("Modal.show()", config);
-
 }
 
 function close() {
