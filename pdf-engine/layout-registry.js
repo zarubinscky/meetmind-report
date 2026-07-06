0992072bfff69bfbcc1eb3e325bc6cdb78ae72b3
zarubinscky <zarubinscky@gmail.com>
2026-07-06 11:41:32 +0300

    Create layout-registry.js

---
+++ pdf-engine/layout-registry.js
@@ -0,0 +1,15 @@
+(function () {
+
+    "use strict";
+
+    const BLOCK_LAYOUT_MODES = {
+
+        statistics: [
+            {
+                id: "default",
+                label: "Default statistics",
+                penalty: 0
+            },
+            {
+                id: "compact",
+                label: "Compact statistics",
