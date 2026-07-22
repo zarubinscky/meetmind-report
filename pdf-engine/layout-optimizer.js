ea9b09be033f8be07e3ada8170c583a75531c308
zarubinscky <zarubinscky@gmail.com>
2026-07-22 17:29:09 +0300

    Create layout-optimizer.js

---
+++ pdf-engine/layout-optimizer.js
@@ -0,0 +1,35 @@
+(function () {
+"use strict";
+function optimize({
+
+    layout,
+
+    measurement,
+
+    layoutModes
+
+}) {
+
+    return {
+
+        changed: false,
+
+        layoutModes,
+
+        reason: "no-op"
+
+    };
+
+}
+
+window.LayoutOptimizer = {
+
+    version: "2.0.0",
+
+    optimize
+
+};
+
+console.log("✅ Layout Optimizer loaded.");
+
+})();
